deabbf33b1c3d1c3625fcfacbec0936fdf97c9d7
Andre Pato <anpato1994@gmail.com>
2019-11-01 15:34:06 -0400

    update redirect

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { Login } from '../screens/Private'
 const AuthenticatedRoute = ({
 	component: Component,
 	children,
@@ -10,14 +11,7 @@ const AuthenticatedRoute = ({
 	if (token && render) {
 		return <Route {...rest} render={render} />
 	} else {
-		return (
-			<Route
-				{...rest}
-				render={(props) =>
-					token ? <Component {...props} /> : <Redirect to='/' />
-				}
-			/>
-		)
+		return <Route {...rest} render={(props) => <Login {...props} />} />
 	}
 }
 
