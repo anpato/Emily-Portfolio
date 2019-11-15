7fae42f7dba3c6ee5d670fce323d8941a9023f40
Andre Pato <anpato1994@gmail.com>
2019-11-15 00:03:22 -0500

    removed log from sign in

--- auth/auth.js
+++ auth/auth.js
@@ -11,100 +11,100 @@ const SaltFactor = parseInt(process.env.SALT_FACTOR)
 
 const SECRET = process.env.APP_SECRET
 
-const signToken = (payload) => jwt.sign(payload, SECRET)
+const signToken = payload => jwt.sign(payload, SECRET)
 
 passport.use(
-	'signup',
-	new LocalStrategy(
-		{
-			passReqToCallback: true,
-			usernameField: 'username',
-			passwordField: 'password'
-		},
-		async (req, username, password, done) => {
-			try {
-				password = bcrypt.hashSync(password, SaltFactor)
-				const user = new User({
-					name: {
-						first: req.body.name.first,
-						last: req.body.name.last
-					},
-					email: req.body.email,
-					username,
-					password
-				})
-				await user.save()
-				done(null, user)
-			} catch (error) {
-				done(error)
-			}
-		}
-	)
+  'signup',
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+      usernameField: 'username',
+      passwordField: 'password'
+    },
+    async (req, username, password, done) => {
+      try {
+        password = bcrypt.hashSync(password, SaltFactor)
+        const user = new User({
+          name: {
+            first: req.body.name.first,
+            last: req.body.name.last
+          },
+          email: req.body.email,
+          username,
+          password
+        })
+        await user.save()
+        done(null, user)
+      } catch (error) {
+        done(error)
+      }
+    }
+  )
 )
 
 passport.use(
-	'login',
-	new LocalStrategy(
-		{
-			usernameField: 'username',
-			passwordField: 'password'
-		},
-		async (username, password, done) => {
-			try {
-				const user = await User.findOne().where({
-					username: username || username.toLowerCase()
-				})
-				console.log(user)
-				if (!user) return done(null, false, { msg: 'User not found' })
+  'login',
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password'
+    },
+    async (username, password, done) => {
+      try {
+        const user = await User.findOne().where({
+          username: username || username.toLowerCase()
+        })
+        // console.log(user)
+        if (!user) return done(null, false, { msg: 'User not found' })
 
-				const authenticateUser = await bcrypt.compare(password, user.password)
-				if (!authenticateUser)
-					return done(null, false, { msg: 'Could not validate password' })
+        const authenticateUser = await bcrypt.compare(password, user.password)
+        if (!authenticateUser)
+          return done(null, false, { msg: 'Could not validate password' })
 
-				return done(null, user, { msg: 'User Authenticated' })
-			} catch (error) {
-				return done(error)
-			}
-		}
-	)
+        return done(null, user, { msg: 'User Authenticated' })
+      } catch (error) {
+        return done(error)
+      }
+    }
+  )
 )
 
 passport.use(
-	new JWTStrategy(
-		{
-			secretOrKey: SECRET,
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-		},
-		async (token, done) => {
-			try {
-				const user = await User.findById(token.id)
-				user ? done(null, user) : done(null, false)
-			} catch (error) {
-				done(error)
-			}
-		}
-	)
+  new JWTStrategy(
+    {
+      secretOrKey: SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    },
+    async (token, done) => {
+      try {
+        const user = await User.findById(token.id)
+        user ? done(null, user) : done(null, false)
+      } catch (error) {
+        done(error)
+      }
+    }
+  )
 )
 
 const userAuthorized = (req, res, next) => {
-	passport.authenticate('jwt', { session: false }, async (error, token) => {
-		if (error || !token) {
-			let err = new Error('Could not authenticate!')
-			err.status = 401
-			next(err)
-		}
-		try {
-			const user = await User.findById(token.id)
-			req.user = user
-		} catch (error) {
-			next(error)
-		}
-		next()
-	})(req, res, next)
+  passport.authenticate('jwt', { session: false }, async (error, token) => {
+    if (error || !token) {
+      let err = new Error('Could not authenticate!')
+      err.status = 401
+      next(err)
+    }
+    try {
+      const user = await User.findById(token.id)
+      req.user = user
+    } catch (error) {
+      next(error)
+    }
+    next()
+  })(req, res, next)
 }
 
 module.exports = {
-	userAuthorized,
-	passport,
-	signToken
+  userAuthorized,
+  passport,
+  signToken
 }
